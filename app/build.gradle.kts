67d3bcf9d4324ae36f26cb1a2854d98843ea66ac
Robbie Espaldon <espaldonrobbie88@gmail.com>
2025-04-30 15:18:51 +0800

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.scical"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.scical"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+    // MXParser with exclusion
+    implementation(libs.mxparser) {
+        exclude(group = "com.android.support")
+    }
+
+    implementation(libs.gridlayout)
+
+    // GraphView with exclusion
+    implementation(libs.graphview) {
+        exclude(group = "com.android.support")
+    }
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
